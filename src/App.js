e04cab94028848384bec9914aa323d36e0170e95
ihsanalgül <algulihsan@hotmail.com>
2020-12-27 21:33:29 +0300

    Login and register pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import React from "react";
 import {BrowserRouter as Router ,Switch , Route} from "react-router-dom";
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
 import Home from './home/Home';
+import Login from "./login/Login";
 
 
 function App() {
@@ -12,7 +14,7 @@ function App() {
       <div className="App">
       <Header />
       <Switch>
-      <Route path="/login"><h1> Login</h1>  </Route>
+      <Route path="/login"> <Login/> </Route>
       <Route path="/logout"><h1> Logout</h1> </Route>
       <Route path="/register"><h1> Register </h1></Route>
       <Route path="/" component={Home}/>
